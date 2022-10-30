1a008d3fd8a673b343882b6eaed5ade8e4bcb7ee
Ivan <stepanyik@etcetera.kiev.ua>
2022-10-30 14:24:59 +0200

    Update: added MVC

--- admin/settings/General.php
+++ admin/settings/General.php
@@ -1,52 +1,19 @@
 <?php
 
 /**
- * VSFB_General class will create the page to load the table
+ * VSFB_Settings_General class will create the page to load the table
  */
 
-class VSFB_General
+class VSFB_Settings_General
 {
     /**
      * Get the table data
      *
-     * @return String
+     * @return HTML
      */
 
     public function view()
-    { ?> 
-        <form>
-            <table class='form-table' role='presentation'>
-                <tbody>
-                    <tr class='form-field form-required'>
-                        <th scope='row'><label for='title'>Title<span class='description'> *</span></label></th>
-                        <td><input name='title' type='text' aria-required='true' maxlength='60'></td>
-                    </tr>
-                    <tr class='form-field'>
-                        <th scope='row'><label for='desc'>Description<span class='description'></span></label></th>
-                        <td><textarea name='desc'></textarea></td>
-                    </tr>
-                    <tr class='form-field'>
-                        <th scope='row'><label for='default_category'>Type<span class='description'></span></label></th>
-                        <td>
-                            <select name='default_category' id='default_category' class='postform'>
-                                <option class='level-0' value='1' selected='selected'>Без рубрики</option>
-                                <option class='level-1' value='2'>Без рубрики 2</option>
-                            </select>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr>
-        </form>
-        <form>
-            <table class='form-table' role='presentation'>
-                <tbody>
-                    <tr class='form-field'>
-                        <th scope='row'><button name='update_db' class='button button-primary' onclick='confirm('Are you sure?')'>Update DB</button></th>
-                        <td> Після цього база данних обновиться!</td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
-    <? }
+    { 
+        require_once (ABSPATH . 'wp-content/plugins/form-builder/admin/views/settings/general.php');
+    }
 }
--- admin/settings/index.php
+++ admin/settings/index.php
@@ -2,31 +2,41 @@
 require_once ( ABSPATH . 'wp-content/plugins/form-builder/inc/Tabs.php' );
 require_once __DIR__ . '/General.php';
 require_once __DIR__ . '/Email.php';
+require_once __DIR__ . '/Styles.php';
 
-$General = new VSFB_General();
-$Email   = new VSFB_Email();
+/**
+ * VSFB_Settings class will create the page to load the settings
+ */
 
-$Tabs = new VSFB_Tabs('form-builder-settings', [
-    [ 'title' => 'General', 'content' => $General ],
-    [ 'title' => 'E-mail', 'content' => $Email ],
-]);
+class VSFB_Settings
+{
+	/**
+	 * This is our constructor
+	 *
+	 * @return void
+	 */
 
-?>
+	public function __construct() 
+	{
+        $Tabs = new VSFB_Tabs('form-builder-settings', [
+            [ 'title' => 'General', 'content' => new VSFB_Settings_General() ],
+            [ 'title' => 'Styles', 'content' => new VSFB_Settings_Styles() ],
+            [ 'title' => 'E-mail', 'content' => new VSFB_Settings_Email() ],
+        ]);
 
+        $this->view($Tabs);
+	}
+
+    /**
+     * Get the table data
+     *
+     * @return String
+     */
+
+    public function view($Tabs)
+    { 
+        require_once (ABSPATH . 'wp-content/plugins/form-builder/admin/views/settings/index.php');
+    }
+}
     
-
-<div class="privacy-settings-header" style="margin-left: -20px">
-    <div class="privacy-settings-title-section">
-        <h1><?= esc_html( get_admin_page_title() ) ?></h1>
-    </div>
-
-    <nav class="privacy-settings-tabs-wrapper hide-if-no-js" aria-label="Secondary menu">
-        <? $Tabs->nav() ?>
-    </nav>
-</div>
-
-<div class="tab-content">
-    <? $Tabs->content()->view() ?>
-</div>
-
-
+new VSFB_Settings();
